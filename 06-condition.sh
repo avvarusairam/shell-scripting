460e6338180dd7a93e707c13237d052ed594fd2f
avvarusairam <avvarusairam123@gmail.com>
2021-12-13 11:29:29 +0530

    conditions

--- 06-condition.sh
+++ 06-condition.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 read  -p 'Enter you age: ' age
+if [ -z "$age" ]; then
+  echo input missing
+  exit
+ fi
 if [ "${age}" -lt 18 ]; then
 echo you are a minor
 elif [ "${age}" -gt 60 ]; then
